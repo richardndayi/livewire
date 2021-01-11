0fe90e052c596b18c766f7f225039e5ce0f8cebd
richardndayi <ndaricad@gmail.com>
2021-01-11 07:59:40 -0500

    ADD FOR AUTHENTIFICATION

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return  view('/home.index');
+}); */
+
+Route::get('/', function () {
+    return view('welcome');
 });
+
+//Route::get('categoriecomptes',Categoriecomptes::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('/home.index');
+})->name('/home.index');
